3b3b7d49d5a96cc585cdc9f6e44295f484c5e8b3
Zerimar <88523858+AbyZerimar11@users.noreply.github.com>
2025-04-29 17:43:25 -0600

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE =https:"//cnt-0a2a7432-7359-49df-ba24-2028d6b9de10.containerhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
+URL_SERVICE ="https://cnt-0a2a7432-7359-49df-ba24-2028d6b9de10.containerhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
 CREATE_USER_PATH = "/api/v1/users/" # Almacena la ruta para crear un usuario o usuaria en esta variable
 CREATE_KIT_PATH = "/api/v1/kits/" # Almacena la ruta para crear un kit en esta variable
